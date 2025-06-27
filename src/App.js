b1ced9e8c3837d412ea2d68a7a9265cf1b76656d
kureti Namah sivaya <kuretinamahsivaya7@gmail.com>
2025-06-27 09:44:43 +0530

    added code to main

--- src/App.js
+++ src/App.js
@@ -4,8 +4,33 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import PrivateChat from './pages/PrivateChat';
 import LoginSignup from './components/LoginSignup';
 import EmptyPage from './components/EmptyPage';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Stop automatic prompt
+      // e.preventDefault();
+      deferredPrompt = e;
+
+      // Prompt immediately or after timeout
+      setTimeout(() => {
+        deferredPrompt.prompt();
+
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+          } else {
+            console.log('User dismissed the install prompt');
+          }
+          deferredPrompt = null;
+        });
+      }, 3000); // delay for UX
+    });
+  }, []);
+
   return (
     <div className="App">
      {/* <ChatUi/> */}
